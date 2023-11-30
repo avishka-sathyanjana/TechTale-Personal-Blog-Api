e9a460cee4ae66c001f45070f00bf321ee98250c
Avishka Sathyanjana <baavishka@gmail.com>
2023-11-30 21:26:54 +0530

    feat: add view posts functionality in  backend

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+
 const User =  require('./models/user');
+const Post = require('./models/Post')
+
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
 const jwt =  require('jsonwebtoken');
+
+
 const cookieParser = require('cookie-parser');
+
+//to handle uploading files
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+
+//to rename the file - file system
+const fs = require('fs');
 
 const app = express();
 
@@ -26,6 +38,8 @@ app.use(express.json());
 // to read the cookies
 app.use(cookieParser());
 
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 mongoose.connect('mongodb+srv://admin:2000@cluster0.exa1cma.mongodb.net/?retryWrites=true&w=majority');
 
 // app.post('/register', async (req,res) => {
@@ -33,7 +47,7 @@ mongoose.connect('mongodb+srv://admin:2000@cluster0.exa1cma.mongodb.net/?retryWr
 //     const userDoc = await User.create({username, password});
 //     res.json(userDoc);
     
-// });
+// }); 
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
@@ -49,26 +63,24 @@ app.post('/register', async (req,res) => {
     }
   });
 
-app.post('/login', async (req, res) =>{
-    const {username, password} = req.body;
-    const userDoc = await User.findOne({username})
-    // console.log(userDoc)
-    const passOk = bcrypt.compareSync(password, userDoc.password)
-    
+  app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-        // logged in response with json token
-        //        the things that need to include - our secret key - options
-        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
-          if (err) throw err;
-          res.cookie('token', token).json({
-            id:userDoc._id,
-            username,
-          });
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
         });
-      } else {
-        res.status(400).json('wrong credentials');
-      }
-    });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+
 
   // to get the profile information
   app.get('/profile', (req,res) => {
@@ -85,6 +97,50 @@ app.post('/login', async (req, res) =>{
   app.post('/logout', (req,res) => {
     // set the token to an empty string
     res.cookie('token', '').json('ok');
+  });
+
+
+  //create a post
+  app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+    
+    //to grab the file extension
+    const {originalname,path} = req.file;
+    //splitting file
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    
+    // rename with file extension
+    fs.renameSync(path, newPath);
+  
+    const {token} = req.cookies;
+    
+    jwt.verify(token, secret, {}, async (err,info) => {
+      if (err) throw err;
+      const {title,summary,content} = req.body;
+
+      //creating the post in mongodb
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+        author:info.id,
+      });
+      res.json(postDoc);
+    });
+  
+  });
+
+
+  //view all the posts
+  app.get('/post', async (req,res) => {
+    res.json(
+      await Post.find()
+        .populate('author', ['username'])
+        .sort({createdAt: -1})
+        .limit(20)
+    );
   });
 
 app.listen(4000);
